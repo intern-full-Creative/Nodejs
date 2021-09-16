15587533b0db7374631487fab83b8c3bccb52a6b
Jith <jith4j@gmail.com>
2021-09-16 14:52:26 +0530

    changed importing and exporting modules

--- getting-started/demo_module.js
+++ getting-started/demo_module.js
@@ -1,11 +1,12 @@
 var http = require("http");
+var dt = require("./myFirstModule");
 
 http
   .createServer((req, res) => {
     res.writeHead(200, {
       "Content-Type": "text/html",
     });
-    res.end("Hello World to Node!");
+    res.write("The date and time are currently: " + dt.myDateTime());
   })
   .listen(8080, () => {
     console.log("Server running");
